98df4ca4eaec6af0b8376b2dcd5a95fdf6ec33ab
Thanadol055 <155058243+Thanadol055@users.noreply.github.com>
2024-11-19 14:26:24 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.title("😊😊Website Devloping using Python😊😊")
-#st.header("😊😊Website Devloping using Python😊😊")
+st.header("😊😊Website Devloping using Python😊😊")
 
 st.subheader("Thanadol Phonin")
