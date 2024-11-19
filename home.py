88e2ef2cc139d4c01bf9b9a86de4d5c7f42089ed
Thanadol055 <155058243+Thanadol055@users.noreply.github.com>
2024-11-19 14:54:54 +0700

    .

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("😊😊Website Devloping using Python😊😊")
 st.header("😊😊Website Devloping using Python😊😊")
 
-st.image('./img/Thanadol.png')
+st.image('./img/Photo.jpg')
 st.subheader("Thanadol Phonin")
 
 dt=pd.read_csv('./data/iris.csv')
